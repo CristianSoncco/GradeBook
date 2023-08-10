312e2de70af18d466f878cea1ab7ea3dc1fb19f7
Cristian Soncco <cristian_soncco@msn.com>
2023-08-09 21:03:34 -0500

    interface, refactoring.

--- src/GradebookRegards/Book.cs
+++ src/GradebookRegards/Book.cs
@@ -1,5 +1,9 @@
+using GradebookRegards;
+using System.IO;
+
 namespace GradebookRegards
 {
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
     public class NameOfObject
     {
         public NameOfObject(string name)
@@ -13,13 +17,109 @@ namespace GradebookRegards
         }
     }
 
-    public class Book : NameOfObject
+    public interface IBook
     {
-        public delegate void GradeAddedDelegate(object sender, EventArgs args);
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        string Name{get;} 
+        event GradeAddedDelegate GradeAdded;
+    }
+    public abstract class Book : NameOfObject, IBook
+    {
         public Book(string name) : base(name)
+        {
+
+        }
+        public abstract event GradeAddedDelegate GradeAdded;
+        public abstract void AddGrade(double grade);
+
+        public abstract Statistics GetStatistics();
+
+    }
+    public class DiskBook : Book
+    {
+        public DiskBook(string name): base(name)
+        {
+
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+        public override void AddGrade(double grade)
+        {   
+            using (var writer   =   File.AppendText($"{Name}.txt"))
+            {
+                writer.WriteLine(grade);
+                if (GradeAdded  != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            // var writer  =   File.AppendText($"{Name}.txt");
+            // writer.WriteLine(grade);
+            // writer.Dispose();
+        } 
+
+
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            using (var  reader  =   File.OpenText($"{Name}.txt"))
+            {
+                var line    =   reader.ReadLine();
+                while (line!=null)
+                {
+                    var number=double.Parse(line);
+                    result.Add(number);
+                    line=reader.ReadLine();
+                }
+            }
+            return  result;
+        }
+    }
+    public class InMemoryBook : Book 
+    {
+
+        public InMemoryBook(string name) : base(name)
         {
             grades= new List<double>();
             Name   =   name;
+        }
+
+
+        public override void AddGrade(double grade)
+        {
+            if(grade<=100 && grade>=0)
+            {
+                grades.Add(grade);
+                if(GradeAdded!=null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override Statistics  GetStatistics()
+        {
+            var result  =   new Statistics();
+
+            for (int index = 0; index < grades.Count; index +=1)
+            {
+                result.Add(grades[index]);
+                // if (grades[index]   ==  0)
+                // {
+                //     goto done;
+                // }
+
+            }
+
+            done:
+            return result;
         }
 
         public void AddGrade(char letter)
@@ -43,70 +143,8 @@ namespace GradebookRegards
                     break;
             }
         }
-        public void AddGrade(double grade)
-        {
-            if(grade<=100 && grade>=0)
-            {
-                grades.Add(grade);
-                if(GradeAdded!=null)
-                {
-                    GradeAdded(this, new EventArgs());
-                }
-            }
-            else
-            {
-                throw new ArgumentException($"Invalid {nameof(grade)}");
-            }
-        }
 
-        public event GradeAddedDelegate GradeAdded;
-
-        public Statistics  GetStatistics()
-        {
-            var result  =   new Statistics();
-            result.Average  =   0.0;
-            result.High   =   double.MinValue;
-            result.Low   =   double.MaxValue;
-
-            for (int index = 0; index < grades.Count; index +=1)
-            {
-                if (grades[index]   ==  0)
-                {
-                    goto done;
-                }
-                result.Low  =   Math.Min(grades[index],result.Low);
-                result.High =   Math.Max(grades[index],result.High);
-                result.Average  =  result.Average + grades[index];
-            }
-
-
-            result.Average = result.Average/grades.Count;
-
-            switch (result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
-
-            done:
-            return result;
-        }
         private List<double> grades;
-
-
 
         public const string CATEGORY = "Science";
     }
